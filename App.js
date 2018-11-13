1561945554899f8c90afaa252c5d077a5feafad5
Mojtaba <mojtabamarashee@gmail.com>
2018-11-13 11:58:52 +0330

    Fixed with eslint

--- App.js
+++ App.js
@@ -6,80 +6,80 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import {Button} from 'react-native';
-import SmsListener from 'react-native-android-sms-listener';
-import {PermissionsAndroid} from 'react-native';
-import RNFetchBlob from 'rn-fetch-blob';
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View } from 'react-native'
+import { Button } from 'react-native'
+import SmsListener from 'react-native-android-sms-listener'
+import { PermissionsAndroid } from 'react-native'
+import RNFetchBlob from 'rn-fetch-blob'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
+})
 
-type Props = {};
+type Props = {}
 async function requestReadSmsPermission() {
   try {
     const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_SMS, {
       title: 'Auto Verification OTP',
       message: 'need access to read sms, to verify OTP',
-    });
+    })
     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log('sms read permissions granted', granted);
+      console.log('sms read permissions granted', granted)
     } else {
-      console.log('sms read permissions denied', denied);
+      console.log('sms read permissions denied')
     }
   } catch (err) {
-    console.warn(err);
+    console.warn(err)
   }
 }
 
 export default class App extends Component<Props> {
   constructor(props) {
-    super(props);
-    this.SMSReadSubscription = {};
-    this.state = {mes: 2};
+    super(props)
+    this.SMSReadSubscription = {}
+    this.state = { mes: 2 }
   }
 
   componentDidMount() {
-    //requestReadSmsPermission();
+    // requestReadSmsPermission();
 
-    const dirs = RNFetchBlob.fs.dirs;
+    const { dirs } = RNFetchBlob.fs
 
     RNFetchBlob.fs
-      .mkdir(dirs.DownloadDir + '/../testFsBlob')
+      .mkdir(`${dirs.DownloadDir}/../testFsBlob`)
       .then(console.log('dir created'))
       .catch(err => {
-        this.setState({mes: err.toString()});
-      });
+        this.setState({ mes: err.toString() })
+      })
 
     RNFetchBlob.fs
-      .appendFile(dirs.DownloadDir + '/../testFsBlob.txt', 'foo', 'utf8')
+      .appendFile(`${dirs.DownloadDir}/../testFsBlob.txt`, 'foo', 'utf8')
       .then(console.log('file created'))
       .catch(err => {
-        this.setState({mes: err.toString()});
-      });
+        this.setState({ mes: err.toString() })
+      })
 
     this.SMSReadSubscription = SmsListener.addListener(message => {
-      console.log('Message:', message);
-      this.setState({mes: message.originatingAddress});
-      let reg = new RegExp('\\d+');
-      let matches = message.body.match(reg);
-      this.setState({mes: message.originatingAddress + matches});
-    });
+      console.log('Message:', message)
+      this.setState({ mes: message.originatingAddress })
+      const reg = new RegExp('\\d+')
+      const matches = message.body.match(reg)
+      this.setState({ mes: `from : ${message.originatingAddress}Nums : ${matches}` })
+    })
   }
 
   componentWillUnmount() {
-    //remove listener
-    this.SMSReadSubscription.remove();
+    // remove listener
+    this.SMSReadSubscription.remove()
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Button
-          onPress={this.componentDidMountt}
+          onPress={this.componentDidMount}
           title="Learn More"
           color="#841584"
           accessibilityLabel="Learn more about this purple button"
@@ -89,7 +89,7 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>{this.state.mes}</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -98,7 +98,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FC00',
+    backgroundColor: '#F5FC22',
   },
   welcome: {
     fontSize: 20,
@@ -110,4 +110,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
