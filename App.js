13008199e947751419605f017b68dfdeace76a12
Mojtaba <mojtabamarashee@gmail.com>
2018-11-13 17:03:57 +0330

    add interval task

--- App.js
+++ App.js
@@ -2,78 +2,98 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
- * @format
  * @flow
  */
 
-import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
-import { Button } from 'react-native'
-import SmsListener from 'react-native-android-sms-listener'
-import { PermissionsAndroid } from 'react-native'
-import RNFetchBlob from 'rn-fetch-blob'
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
+import SmsListener from 'react-native-android-sms-listener';
+import { PermissionsAndroid } from 'react-native';
+import RNFetchBlob from 'rn-fetch-blob';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-})
+});
 
-type Props = {}
+type Props = {};
+
 async function requestReadSmsPermission() {
   try {
     const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_SMS, {
       title: 'Auto Verification OTP',
       message: 'need access to read sms, to verify OTP',
-    })
+    });
     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log('sms read permissions granted', granted)
+      console.log('sms read permissions granted', granted);
     } else {
-      console.log('sms read permissions denied')
+      console.log('sms read permissions denied');
     }
   } catch (err) {
-    console.warn(err)
+    console.warn(err);
   }
 }
 
 export default class App extends Component<Props> {
   constructor(props) {
-    super(props)
-    this.SMSReadSubscription = {}
-    this.state = { mes: 2 }
+    super(props);
+    this.SMSReadSubscription = {};
+    this.state = { mes: 2 };
   }
 
   componentDidMount() {
-    // requestReadSmsPermission();
+    requestReadSmsPermission();
 
-    const { dirs } = RNFetchBlob.fs
+    const { dirs } = RNFetchBlob.fs;
 
     RNFetchBlob.fs
       .mkdir(`${dirs.DownloadDir}/../testFsBlob`)
       .then(console.log('dir created'))
       .catch(err => {
-        this.setState({ mes: err.toString() })
-      })
-
-    RNFetchBlob.fs
-      .appendFile(`${dirs.DownloadDir}/../testFsBlob.txt`, 'foo', 'utf8')
-      .then(console.log('file created'))
-      .catch(err => {
-        this.setState({ mes: err.toString() })
-      })
+        this.setState({ mes: err.toString() });
+      });
 
     this.SMSReadSubscription = SmsListener.addListener(message => {
-      console.log('Message:', message)
-      this.setState({ mes: message.originatingAddress })
-      const reg = new RegExp('\\d+')
-      const matches = message.body.match(reg)
-      this.setState({ mes: `from : ${message.originatingAddress}Nums : ${matches}` })
-    })
+      console.log('Message:', message);
+      this.setState({ mes: message.originatingAddress });
+      const reg = new RegExp('\\d+');
+      const matches = message.body.match(reg);
+      this.setState({ mes: `from : ${message.originatingAddress}Nums : ${matches}` });
+    });
+
+    this.timer = setInterval(() => {
+      console.log('I do not leak!');
+      this.CreateFile();
+    }, 1000);
   }
 
   componentWillUnmount() {
     // remove listener
-    this.SMSReadSubscription.remove()
+    this.SMSReadSubscription.remove();
+    clearInterval(this.timer);
   }
+
+  CreateFile = () => {
+    const { dirs } = RNFetchBlob.fs;
+    RNFetchBlob.fs
+      .appendFile(`${dirs.DownloadDir}/../testFsBlob.txt`, 'foo', 'utf8')
+      .then(console.log('file created'))
+      .catch(err => {
+        this.setState({ mes: err.toString() });
+      });
+  };
+
+  RunAt10am = () => {
+    const now = new Date();
+    let millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 0, 0, 0) - now;
+    if (millisTill10 < 0) {
+      millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
+    }
+    setTimeout(() => {
+      alert("It's 10am!");
+    }, millisTill10);
+  };
 
   render() {
     return (
@@ -89,7 +109,7 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>{this.state.mes}</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
-    )
+    );
   }
 }
 
@@ -110,4 +130,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-})
+});
