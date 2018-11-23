e8d3e218cbba90dfe78c1da75ef6add2e207e29b
unknown <mojtabamarashee@gmail.com>
2018-11-24 00:29:26 +0330

    add redux

--- Components/Search.js
+++ Components/Search.js
@@ -17,12 +17,27 @@ import { List, ListItem, SearchBar } from 'react-native-elements';
 
 const PersianCalendarPicker = require('react-native-persian-calendar-picker');
 
- 
 export default class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showCal: false, date: 'init' };
+    this.SetDate = this.SetDate.bind(this);
+    this.SetDate1 = this.SetDate1.bind(this);
+  }
+
+  SetDate(date) {
+    this.setState({ showCal: false });
+	  date1 = date.format('jYY/jMM/jDD');
+    this.setState({date:date1.toString()});
+  }
+
+  SetDate1() {
+    this.setState({ showCal: true });
+  }
   render() {
-    return (
+    return this.state.showCal ? (
       <View lightTheme>
-        <PersianCalendarPicker allowRangeSelection={true} onDateChange={this.onDateChange} />
+        <PersianCalendarPicker allowRangeSelection={false} onDateChange={this.SetDate} />
         <SearchBar onChangeText={text => this.searchFilterFunction(text)} placeholder="Type Here..." lightTheme round />
         <Text
           style={{
@@ -37,6 +52,13 @@ export default class Search extends Component {
           total : {1}
         </Text>
       </View>
+    ) : (
+      <Button
+        onPress={this.SetDate1}
+        title={this.state.date.toString()}
+        color="#841584"
+        accessibilityLabel="Lear more about this purple button"
+      />
     );
   }
 }
